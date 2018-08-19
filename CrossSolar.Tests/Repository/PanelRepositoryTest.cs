4f1d10a7bab741f91b4a37229dd118485de37caf
Domício Medeiros <domicioam@gmail.com>
2018-08-19 00:10:27 -0300

    Creating unit test to cover PanelRepository.InsertAsync() and PanelRepository.GetAsync().

---
+++ CrossSolar.Tests/Repository/PanelRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using CrossSolar.Domain;
+using CrossSolar.Repository;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CrossSolar.Tests.Repository
+{
+    public class PanelRepositoryTest
+    {
+      public PanelRepositoryTest()
+      {
+         _panelRepository = _panelRepositoryMock.Object;
+      }
+
+      private readonly Mock<IPanelRepository> _panelRepositoryMock = new Mock<IPanelRepository>();
+      private readonly IPanelRepository _panelRepository;
+
+
+      [Fact]
+      public async void InsertAsync_ShouldInsertPanel()
+      {
+         Panel panel = new Panel()
+         {
+            Brand = "Areva",
+            Latitude = 12.345678,
+            Longitude = 98.7655432,
+            Serial = "AAAA1111BBBB2222"
+         };
+
+         panel.Id = await _panelRepository.InsertAsync(panel);
+         var insertedPanel = await _panelRepository.GetAsync(panel.Id.ToString());
+         Assert.NotNull(insertedPanel);
+      }
+   }
+}
